dcc5e8c30087a0d0da62d190a2500477c60c7cd5
Gohlking <102523153+Gohlking@users.noreply.github.com>
2022-04-21 14:25:14 +0200

    Add files via upload

---
+++ Lab1/insert.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?> 
+
+<h1>Hier kannst du Sachen Posten</h1>
+
+</body>
+</html>
---
+++ Lab1/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs');
+const port = 3000
+
+const server = http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('login.html', function (error, data) {
+        if (error) {
+            response.writeHead(404)
+            response.write('Error: File Not found')
+        } else {
+            response.write(data)
+        }
+        response.end();
+    })
+})
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Server is running on Port ' + port)
+    }
+})
