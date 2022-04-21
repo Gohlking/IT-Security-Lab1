398a8497b8ce317f61c7db9545a20a0b55c8f155
dugoit00 <dugoit00@hs-esslingen.de>
2022-04-21 15:52:50 +0200

    commit

--- Lab1/server.js
+++ Lab1/server.js
@@ -2,6 +2,8 @@ const http = require('http')
 const fs = require('fs');
 const port = 3000
 
+var mysql = require('mysql');
+
 const server = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' })
     fs.readFile('login.html', function (error, data) {
@@ -15,6 +17,23 @@ const server = http.createServer(function (request, response) {
     })
 })
 //
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'local',
+    database: 'users'
+});
+
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
 //
 server.listen(port, function (error) {
     if (error) {
