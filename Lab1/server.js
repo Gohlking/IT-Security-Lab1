05a5f984501a38dac9b257f3fbbe424bf1fb6d81
dugoit00 <dugoit00@hs-esslingen.de>
2022-04-22 16:09:02 +0200

    Login page working

--- Lab1/server.js
+++ Lab1/server.js
@@ -1,21 +1,14 @@
 const http = require('http')
 const fs = require('fs');
-const port = 3000
+
+
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
 
 var mysql = require('mysql');
 
-const server = http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('login.html', function (error, data) {
-        if (error) {
-            response.writeHead(404)
-            response.write('Error: File Not found')
-        } else {
-            response.write(data)
-        }
-        response.end();
-    })
-})
+
 //
 
 var connection = mysql.createConnection({
@@ -25,26 +18,74 @@ var connection = mysql.createConnection({
     database: 'itsecurity'
 });
 
-
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected to Database!");
 });
 
-var x = document.getElementById("")
+const app = express();
 
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
 
-connection.query('SELECT * FROM accounts  ', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-connection.end();
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.redirect('/');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+        response.sendFile(path.join(__dirname + '/home.html'));
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+});
+
+
+
+
+
+
+app.listen(3000);
+
 //
-server.listen(port, function (error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    } else {
-        console.log('Server is running on Port ' + port)
-    }
-})
